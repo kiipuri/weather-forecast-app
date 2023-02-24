ab4f834337e6ea0372b63ff48c35f651d7e23514
kiipuri <kiipuri01@gmail.com>
2023-02-24 12:53:18 +0200

    missing changes from last commit

--- ClientApp/src/app/modify-data/modify-data.component.ts
+++ ClientApp/src/app/modify-data/modify-data.component.ts
@@ -1,28 +1,31 @@
-import { Component, Inject } from "@angular/core";
+import { Component, Inject, Input } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
+import { CustomDateParserFormatter } from "../date-parser-formatter";
+import { CustomDateAdapter } from "../date-parser-formatter";
 
-import { NgbDatepickerModule, NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import {
+  NgbDateAdapter,
+  NgbDateParserFormatter,
+  NgbDatepickerModule,
+  NgbModal,
+} from "@ng-bootstrap/ng-bootstrap";
 import { CommonModule } from "@angular/common";
 
 @Component({
   selector: "app-modify-data-component",
   standalone: true,
   imports: [NgbDatepickerModule, FormsModule, CommonModule],
+  providers: [
+    { provide: NgbDateAdapter, useClass: CustomDateAdapter },
+    { provide: NgbDateParserFormatter, useClass: CustomDateParserFormatter },
+  ],
   templateUrl: "./modify-data.component.html",
 })
 export class ModifyDataComponent {
   closeResult = "";
   baseUrl = "";
-
-  weather: Weather = {
-    id: 0,
-    temperature: 0,
-    rain: 0,
-    wind: 0,
-    date: new Date(Date.now()).toISOString().split("T")[0],
-  };
-
+  weather: Weather = new Weather();
   forecasts: Weather[] = [];
 
   constructor(
@@ -35,20 +38,24 @@ export class ModifyDataComponent {
   }
 
   open(content: any) {
+    this.weather = new Weather();
     this.modalService
       .open(content, { ariaLabelledBy: "modal-basic-title" })
-      .result.then(() => {
-        this.http
-          .post<Weather>(this.baseUrl + "weather", this.weather)
-          .subscribe({
-            next: () => {
-              this.getForecasts();
-            },
-            error: err => {
-              console.error(err);
-            },
-          });
-      });
+      .result.then(
+        () => {
+          this.http
+            .post<Weather>(this.baseUrl + "weather", this.weather)
+            .subscribe({
+              next: () => {
+                this.getForecasts();
+              },
+              error: err => {
+                console.error(err);
+              },
+            });
+        },
+        () => {}
+      );
   }
 
   getForecasts() {
@@ -58,12 +65,38 @@ export class ModifyDataComponent {
       },
     });
   }
+
+  editForecast(weather: Weather, editmodal: any) {
+    Object.assign(this.weather, weather);
+    this.modalService.open(editmodal).result.then(
+      () => {
+        this.http
+          .put<Weather>(this.baseUrl + "weather", this.weather)
+          .subscribe({
+            next: () => {
+              this.getForecasts();
+            },
+          });
+      },
+      () => {}
+    );
+  }
+
+  deleteForecast(weather: Weather) {
+    this.http
+      .delete<Weather>(this.baseUrl + "weather/Delete/" + weather.id)
+      .subscribe({
+        next: () => {
+          this.getForecasts();
+        },
+      });
+  }
 }
 
-interface Weather {
-  id: number;
-  temperature: number;
-  rain: number;
-  wind: number;
-  date: string;
+class Weather {
+  id: number = 0;
+  temperature: number = 0;
+  rain: number = 0;
+  wind: number = 0;
+  date: string = "";
 }
