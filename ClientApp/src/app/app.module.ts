3e1a5ef49600479911feb69a434de660406044ff
kiipuri <kiipuri01@gmail.com>
2023-03-03 18:31:01 +0200

    delete counter component

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -7,24 +7,17 @@ import { RouterModule } from "@angular/router";
 import { AppComponent } from "./app.component";
 import { NavMenuComponent } from "./nav-menu/nav-menu.component";
 import { HomeComponent } from "./home/home.component";
-import { CounterComponent } from "./counter/counter.component";
 import { ModifyDataComponent } from "./modify-data/modify-data.component";
 import { ForecastsComponent } from "./forecasts/forecasts.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-  ],
+  declarations: [AppComponent, NavMenuComponent, HomeComponent],
   imports: [
     BrowserModule.withServerTransition({ appId: "ng-cli-universal" }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
       { path: "", component: HomeComponent, pathMatch: "full" },
-      { path: "counter", component: CounterComponent },
       { path: "modify-data", component: ModifyDataComponent },
       { path: "forecasts", component: ForecastsComponent },
     ]),
